7618bf9c2b46c007055277257a7c07208c4a42fe
Your Name <you@example.com>
2015-01-30 22:24:18 +0000

    Your message goes here

--- db/migrate/20150130041909_create_poems.rb
+++ db/migrate/20150130041909_create_poems.rb
@@ -1,11 +1,11 @@
 class CreatePoems < ActiveRecord::Migration
   def change
     create_table :poems do |t|
-      t.string = :title
-      a.string = :author
-      s.string = :source
-      t.text = :body
-      t.boolean = :published, default: false
+      t.string  :title
+      t.string  :author
+      t.string  :source
+      t.text  :body
+      t.boolean :published, default: false
       t.timestamps null: false 
     end
   end
